3bd3c6389c0c60c4d250cb0b8d6d3d0ff090edbd
fbarbe05 <57678553+fbarbe05@users.noreply.github.com>
2019-12-11 17:31:37 +0100

    upgrade

--- TreasureBox.java
+++ TreasureBox.java
@@ -51,7 +51,15 @@ public class TreasureBox extends Item
         return specialBox;
     }
     
-
+        /**
+     * Give the special item on the box
+     * @return keys, the list of items on the box
+     */
+     public Key getKey()
+    {
+        return keyBox;
+    }
+    
     /**
      * Give the special item on the box
      * @return keys, the list of items on the box
@@ -61,7 +69,6 @@ public class TreasureBox extends Item
         return specials;
     }
 
-    
     public Lock getLock()
     {
         return lockBox;
