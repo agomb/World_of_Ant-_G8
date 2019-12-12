c4efef3f56ef88e61bd37a2720d44ce0f9ec10bc
Thibault-Crouzet <57678886+Thibault-Crouzet@users.noreply.github.com>
2019-12-12 21:11:05 +0100

    Test de Room, Door and Lock

--- RoomTest.java
+++ RoomTest.java
@@ -3,16 +3,24 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
- * The test class RoomTest.
+ * The test for class Room.
  *
  * @author  Angelique, Dylan
  * @version 13/11/2019
  */
 public class RoomTest
 {
-    Room cuisine;
+    Room room1;
     Room room2;
+    
+    Door testDoor;
+    
+    Key keytest;
+    
+    Lock alock;
     /**
      * Default constructor for test class RoomTest
      */
@@ -29,67 +37,86 @@ public class RoomTest
     @Before
     public void setUp()
     {
-        cuisine = new Room("Cuisine");
+        room1 = new Room("cuisine");
         room2 = new Room("tunnel");
+        keytest = new Key("keytest");
+        alock  = new Lock(keytest);
+        testDoor = new Door(room1,"East",room2,"West");
+        
     }
     @Test
     /**
-     * 
+     *  Test if we can an exit to the room
      */
-    public void testgetExit()
-    {
-        assertEquals(cuisine.getExit(),null);
+    public void testCheckExits()
+    {   
+        List<String> exitlist = new ArrayList<String>();
+        assertEquals( exitlist ,room1.checkExits() );
     }
     
     @Test
     /**
-     * 
+     *  Test if we can an exit to the room
      */
     public void testaddExit()
     {   
-        
-        Lock alock  = new Lock(3);
-        Door theDoor = new Door(cuisine,"south",room2,"north",alock);
-        
-        cuisine.addExit(theDoor, "south");
-        assertEquals(cuisine.getExit(),null);
+        room1.addExit(room1,"East",testDoor);
+        assertNotEquals( null ,room1.checkExits() );
     }
     @Test
     /**
-     * 
+     * Test if we can get the description of the room
      */
     public void testgetDescr()
     {   
-        assertEquals(cuisine.getDescr(),"cuisine");
+        assertEquals(room1.getDescription(),"cuisine");
     }
     
     @Test
     /**
-     * 
+     * Test if we can get the list of item inside the room
      */
     public void testgetItem()
-    {   
-        assertEquals(cuisine.getItem(),null);
+    {   List<Item> listitem = new ArrayList<Item>();
+        assertEquals(room1.getItem(),listitem);
     }
     
     @Test
     /**
-     * 
+     * Test if we can add an item to the room
      */
     public void testaddItem()
-    {   Item anItem = new Item("test");
-        cuisine.addItem(anItem);
-        assertEquals(cuisine.getItem(),null);
+    {   
+        Item anItem = new Item("test");
+        room1.addItem(anItem);
+        assertNotEquals(room1.getItem(),null);
     }
     
-    
+    @Test
     /**
-     * 
+     * Test if we can remove an item inside the room
      */
-    public void testremoveItem(Item anItem)
+    public void testremoveItem()
     {
+        List<Item> listitem = new ArrayList<Item>(); //create an empty list of item
+        Item anItem = new Item("test");
+        room1.addItem(anItem);
+        room1.removeItem(anItem);
         
+        assertEquals(room1.getItem(),listitem);
     }
+    
+    
+    @Test
+    /**
+     *  Test if we can choose an exit of the room
+     */
+    public void testChooseExit()
+    {   
+        room1.addExit(room1,"East",testDoor);
+        assertEquals( testDoor ,room1.chooseExit(room1,"East") );
+    }
+    
     /**
      * Tears down the test fixture.
      *
