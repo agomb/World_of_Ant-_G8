c472a1a69773cbfde226e7f0310567b4326272ce
agomb <57678915+agomb@users.noreply.github.com>
2019-12-12 19:52:43 +0100

    Recup dans la treasure box

--- Doable.java
+++ Doable.java
@@ -118,10 +118,10 @@ public class Doable extends JPanel implements ActionListener //creation class (p
             myGameInterface.getGame().getPlayer().drop();
             myGameInterface.getBar().actualisation(myGameInterface.getGame().getPlayer());
             myGameInterface.getDirection().updateUI();
-             myGameInterface.getBar().updateUI();
-             myGameInterface.getVisual().updateUI();
-             myGameInterface.getInfo().updateInfoBox();
-             myGameInterface.getDoable().updateUI();
+            myGameInterface.getBar().updateUI();
+            myGameInterface.getVisual().updateUI();
+            myGameInterface.getInfo().updateInfoBox();
+            myGameInterface.getDoable().updateUI();
         }
         else if(e.getSource() == myButtonDelivery )
         {
@@ -146,10 +146,15 @@ public class Doable extends JPanel implements ActionListener //creation class (p
              myGameInterface.getGame().getPlayer().pickUpBox(myGameInterface.getGame().getPlayer().getCurrentRoom().getBox());
              myGameInterface.getBar().actualisation(myGameInterface.getGame().getPlayer());
              
+             myGameInterface.showMap(myGameInterface.getVisual());
+             myGameInterface.getVisual().visualizeRoom(myGameInterface.getGame().getPlayer());
+            
              myGameInterface.getDirection().updateUI();
              myGameInterface.getBar().updateUI();
              myGameInterface.getVisual().updateUI();
              myGameInterface.getInfo().updateInfoBox();
+             myGameInterface.getDoable().disableAllButtons();
+             //myGameInterface.getDoable().enableButton();
              myGameInterface.getDoable().updateUI();
         }
         
